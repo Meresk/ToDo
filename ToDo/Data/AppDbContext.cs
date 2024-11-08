5fc7674e9f21c4573f3fe7362fdd8c6393ccaba0
0109-07 <0109-07@0109-07>
2024-11-08 11:25:30 +0300

    Created data models and setting up databases,

---
+++ ToDo/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Models;
+
+namespace ToDo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<User_lw5_02> Users_lw5_02 { get; set; }
+        public DbSet<Note_lw5_02> Notes_lw5_02 { get; set; }
+    }
+}
