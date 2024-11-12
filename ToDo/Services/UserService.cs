a52950488a76745e210048f1432b3e598f58ac99
0109-07 <0109-07@0109-07>
2024-11-12 11:23:28 +0300

    Added Authentication/Authorization

---
+++ ToDo/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using Org.BouncyCastle.Crypto.Generators;
+using ToDo.Data;
+using ToDo.Models;
+
+namespace ToDo.Services
+{
+    public class UserService(AppDbContext context)
+    {
+        private readonly AppDbContext _dbContext = context;
+
+        public User_lw5_02? UserVerify(User_lw5_02 user)
+        {
+            var userExist = _dbContext.Users_lw5_02.FirstOrDefault(u => u.Email == user.Email );
+
+            if (userExist != null && BCrypt.Net.BCrypt.Verify(user.Password, userExist.Password))
+            {
+                return user;
+            }
+
+            return null;
+        }
+    }
+}
