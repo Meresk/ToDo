81a70a5337e1edb919ba345beaf54db7ad7cae03
Dimaso <Dimaso.05@mail.ru>
2024-11-14 01:24:04 +0300

    Edit controler Note, Add functional for Files upload and download, fix size card

--- ToDo/Controllers/NoteController.cs
+++ ToDo/Controllers/NoteController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using ToDo.Data;
 using ToDo.Models;
-using System.Security.Claims;
 
 namespace ToDo.Controllers
 {
@@ -48,7 +43,7 @@ namespace ToDo.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize]
-        public async Task<IActionResult> Create([Bind("Title,Description")] Note_lw9_02 note_lw9_02)
+        public async Task<IActionResult> Create([Bind("Title,Description,File")] Note_lw9_02 note_lw9_02)
         {
             var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
@@ -56,11 +51,24 @@ namespace ToDo.Controllers
             {
                 Title = note_lw9_02.Title,
                 Description = note_lw9_02.Description,
-                UserId = Int32.Parse(userId)
+                UserId = Int32.Parse(userId),
+                File = note_lw9_02.File,
+
             };
 
             if (ModelState.IsValid)
             {
+
+                if (createNote.File != null)
+                {
+                    var fileName = Path.GetFileNameWithoutExtension(createNote.File.FileName) + "_" + Guid.NewGuid().ToString() + Path.GetExtension(createNote.File.FileName);
+                    var filePath = Path.Combine("wwwroot/uploads", fileName);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await createNote.File.CopyToAsync(stream);
+                    }
+                    createNote.FilePatch = "/uploads/" + fileName;
+                }
                 _context.Add(createNote);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -72,12 +80,14 @@ namespace ToDo.Controllers
         [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
             if (id == null)
             {
                 return NotFound();
             }
-
             var note_lw5_02 = await _context.Notes_lw9_02.FindAsync(id);
+
             if (note_lw5_02 == null)
             {
                 return NotFound();
@@ -91,7 +101,7 @@ namespace ToDo.Controllers
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,UserId")] Note_lw9_02 note_lw9_02)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,UserId,File")] Note_lw9_02 note_lw9_02)
         {
             if (id != note_lw9_02.Id)
             {
@@ -102,6 +112,32 @@ namespace ToDo.Controllers
             {
                 try
                 {
+                    if (note_lw9_02.File != null)
+                    {
+                        // Генерация уникального имени файла
+                        var fileName = Path.GetFileNameWithoutExtension(note_lw9_02.File.FileName) + "_" + Guid.NewGuid().ToString() + Path.GetExtension(note_lw9_02.File.FileName);
+
+                        // Путь для сохранения файла
+                        var uploadsDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+
+                        // Проверка существования директории и создание, если она не существует
+                        if (!Directory.Exists(uploadsDirectory))
+                        {
+                            Directory.CreateDirectory(uploadsDirectory);
+                        }
+
+                        var filePath = Path.Combine(uploadsDirectory, fileName);
+
+                        // Сохранение файла на сервере
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await note_lw9_02.File.CopyToAsync(stream);
+                        }
+
+                        // Сохранение пути к файлу в модели
+                        note_lw9_02.FilePatch = "/uploads/" + fileName;
+                    }
+
                     _context.Update(note_lw9_02);
                     await _context.SaveChangesAsync();
                 }
@@ -156,9 +192,38 @@ namespace ToDo.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        // POST: Notes/DeleteFile/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteFile(int id)
+        {
+            var note = await _context.Notes_lw9_02.FindAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            // Удаление файла с сервера
+            if (!string.IsNullOrEmpty(note.FilePatch))
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", note.FilePatch.TrimStart('/'));
+                if (System.IO.File.Exists(filePath))
+                {
+                    System.IO.File.Delete(filePath);
+                }
+                note.FilePatch = null;
+                _context.Update(note);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Edit), new { id = note.Id });
+        }
+
+
         private bool Note_lw5_02Exists(int id)
         {
             return _context.Notes_lw9_02.Any(e => e.Id == id);
         }
     }
+
 }
